51fa35e5894da5b77917d732a24af167e38f5b79
Yves Michel <yvestchale2@gmail.com>
2024-06-20 14:23:53 +0200

    exo 5 corrigé

--- js/exo-5.js
+++ js/exo-5.js
@@ -1,21 +1,36 @@
-export const exo2 = async () => {
+export const exo5 = async () => {
     const url = `https://pokeapi.co/api/v2/pokemon`
 
     const response = await fetch(url);
     const result = await response.json();
     const pokemons =   result.results;
+    console.log(pokemons)
     const listePokemonUl = document.querySelector("#pokemons-list")
-    
+    let innerHTML = ""
+   
+
 
         for (let i = 0; i < pokemons.length; i++) { 
           const pokemonindex = pokemons[i].url.split('/'); 
-           listePokemonUl.innerHTML += `
-           <li>
+          innerHTML += `
+           <li >
             <p>Nom : ${pokemons[i].name}</p>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonindex[pokemonindex.length - 2]}.png" alt="${pokemons[i].name}">
+            <button data-id= ${pokemonindex[pokemonindex.length - 2]}>Details</button>
           </li>
+          
            `
+           
         }
+
+        listePokemonUl.innerHTML= innerHTML;
+
+        const listeButtons = listePokemonUl.querySelectorAll('button');
+        listeButtons.forEach(btn => {
+          btn.addEventListener('click', (e) =>{
+            alert(btn.dataset.id)
+          })
+        });
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import { exo1 } from "./exo-1.js";
 import { exo1Corrige } from "./exo-1-corrige.js";
 import { exo2 } from "./exo-2.js";
+import { exo5 } from "./exo-5.js";
 
-exo2();
+exo5();
